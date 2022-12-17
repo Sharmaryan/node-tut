8257b00e2626fcddfaec78d9f9185e5230c7db23
Aryan Sharma <panditaryan.as@gmail.com>
2022-12-17 19:28:43 +0530

    basic api

---
+++ data.js
@@ -0,0 +1,6 @@
+const data = [
+    { name: 'aryan sharma', profession: 'developer' },
+    { name: 'sharma', profession: 'developer' },
+    { name: 'aryan', profession: 'developer' },
+]
+module.exports = data;
