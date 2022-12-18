e98cc47d629a4dea5c63763c0d7fe9607e326d63
Aryan Sharma <panditaryan.as@gmail.com>
2022-12-18 18:42:06 +0530

    removing extension

--- index.js
+++ index.js
@@ -4,7 +4,17 @@ const path = require('path');
 const publicPath = path.join(__dirname, 'public');
 const app = express();
 
-app.use(express.static(publicPath));
+app.get('/', (_, resp) => {
+    resp.sendFile(`${publicPath}/index.html`);
+});
+
+app.get('/about', (_, resp) => {
+    resp.sendFile(`${publicPath}/about.html`);
+});
+
+app.get('/help', (_, resp) => {
+    resp.sendFile(`${publicPath}/help.html`);
+});
 
 app.listen(5000);
 
