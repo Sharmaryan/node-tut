7659bc731170ddfa07a88d157b582f3458c9bb81
Aryan Sharma <panditaryan.as@gmail.com>
2022-12-18 16:21:55 +0530

    files crud operation

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 const fs = require('fs');
+const path = require('path');
+const dirPath = path.join(__dirname, 'crud');
+const filePath = `${dirPath}/random.txt`;
+
+// write file
+// fs.writeFileSync(filePath,'this is random text file');
+
+// read file
+// fs.readFile(filePath,'utf-8',(err,file) => {
+// console.log(file);
+// });
+
+// update file
+// fs.appendFile(filePath,'updated file with this text',(err) => {
+//     if(!err)
+//     console.log('file update successfully');
+// })
+
+// rename file
+
+// fs.rename(filePath, `${dirPath}/fruit.txt`, (err) => {
+//     if (!err)
+//         console.log('file renamed successfully');
+// })
+
+// delete file
+
+// fs.unlinkSync(`${dirPath}/fruit.txt`);
+
