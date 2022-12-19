3a63897331e209449ee5fa336d963241c1d3a5d8
Aryan Sharma <panditaryan.as@gmail.com>
2022-12-19 11:05:47 +0530

    middleware

--- index.js
+++ index.js
@@ -1,35 +1,23 @@
 const express = require('express');
-const path = require('path');
-
-const publicPath = path.join(__dirname, 'public');
 const app = express();
 
-app.set('view engine','ejs');
+const requestFilter = (req, res, next) => {
+    if (!req.query.age) {
+        res.send('please provide age');
+    }
+    else if(req.query.age < 18){
+        res.send('you cannot access this page')
+    }
+    else {
+        next();
+    }
+}
 
-app.get('/', (_, resp) => {
-    resp.sendFile(`${publicPath}/index.html`);
-});
+app.use(requestFilter);
 
-app.get('/profile', (_, resp) => {
-    const user = {name:'aryan', lastName:'sharma',city:'ghaziabad', skills:['react','js','redux','node']};
-    resp.render('profile',{user});
-});
-
-app.get('/login',(_,resp) => {
-    resp.render('login')
+app.get('/', (req, res) => {
+    res.send('welcome to home page');
 })
-
-app.get('/about', (_, resp) => {
-    resp.sendFile(`${publicPath}/about.html`);
-});
-
-app.get('/help', (_, resp) => {
-    resp.sendFile(`${publicPath}/help.html`);
-});
-
-app.get('*', (_, resp) => {
-    resp.sendFile(`${publicPath}/pagenotfound.html`);
-});
 
 app.listen(5000);
 
