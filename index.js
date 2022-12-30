6d1d9acba94066fc59bef5e966899ba94308396f
Aryan Sharma <panditaryan.as@gmail.com>
2022-12-30 13:01:35 +0530

    db connected

--- index.js
+++ index.js
@@ -1,25 +1,14 @@
-const express = require('express');
-const requestFilter = require('./middleware');
-const app = express();
-const route = express.Router();
+const { MongoClient } = require('mongodb');
+const url = 'mongodb://localhost:27017';
+const client = new MongoClient(url);
+const database = 'E-comm'
 
-route.use(requestFilter);
+const getData = async () => {
+    let result = await client.connect(); 
+    let db = result.db(database);
+    let collection = db.collection('products');
+    let response = await (collection.find({}).toArray())
+    console.log(response);
+}
 
-app.get('/', (req, res) => {
-    res.send('welcome to home page');
-})
-app.get('/users', (req, res) => {
-    res.send('welcome to users page');
-})
-route.get('/about', (req, res) => {
-    res.send('welcome to about page');
-})
-route.get('/contact', (req, res) => {
-    res.send('welcome to contact page');
-})
-
-app.use('/',route)
-
-app.listen(5000);
-
-
+getData();
