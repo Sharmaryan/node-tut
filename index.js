4c23152180854b168bfb65aef1c966339dfacd34
Aryan Sharma <panditaryan.as@gmail.com>
2023-01-01 11:32:32 +0530

    adding to the db

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
 const main = async () => {
-    await mongoose.connect('localhost:27017/E-comm');
-    const ProductSchema = new mongoose.Schema({ name: String });
+    await mongoose.connect('mongodb://localhost:27017/E-comm');
+    const ProductSchema = new mongoose.Schema({ name: String, price: Number, brand:String });
 
     const ProductModel = mongoose.model('products', ProductSchema);
-    let data = new ProductModel({ name: 'laptop', price: 1000000 });
+    let data = new ProductModel({ name: 'laptop', price: 1000000, brand:'new brand' });
     let result = await data.save();
     console.log(result);
 
