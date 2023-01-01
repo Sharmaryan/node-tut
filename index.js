14a29a3c55e3a76e565edad58affa71201580d15
Aryan Sharma <panditaryan.as@gmail.com>
2023-01-01 12:15:37 +0530

    post api

--- index.js
+++ index.js
@@ -1,41 +1,16 @@
-const mongoose = require('mongoose');
+const express = require('express');
+require('./config');
 
-mongoose.connect('mongodb://localhost:27017/E-comm');
-const ProductSchema = new mongoose.Schema({ name: String, price: Number, brand: String });
+const Products = require('./product');
 
-const saveInDB = async () => {
+const app = express();
+app.use(express.json());
 
-    const ProductModel = mongoose.model('products', ProductSchema);
-    let data = new ProductModel({ name: 'laptop', price: 1000000, brand: 'new brand' });
+app.post('/create', async (req, res) => {
+    let data = new Products(req.body);
     let result = await data.save();
-    console.log(result);
+    console.log(result)
+    res.send(result)
+})
 
-
-}
-
-
-const updateInDB = async () => {
-    const Product = mongoose.model('products', ProductSchema);
-    let data = await Product.updateOne({ name: 'laptop' }, { $set: { price: '80000' } });
-}
-// updateInDB();
-
-const deleteInDB = async () => {
-    const Product = mongoose.model('products',ProductSchema);
-    let data = await Product.deleteOne({id:'63b11fea87d970bed2d29e1d'});
-    console.log(data)
-}
-
-
-const findInDB = async () => {
-    const Product = mongoose.model('products',ProductSchema);
-    let data = await Product.find();
-    console.log(data)
-}
-
-
-
-
-
-
-
+app.listen(5000);
