20fa4a73522d42c357de19e28b5363d2f4acd15d
Aryan Sharma <panditaryan.as@gmail.com>
2023-01-01 11:41:53 +0530

    update

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const mongoose = require('mongoose');
 
-const main = async () => {
+const saveInDB = async () => {
     await mongoose.connect('mongodb://localhost:27017/E-comm');
-    const ProductSchema = new mongoose.Schema({ name: String, price: Number, brand:String });
+    const ProductSchema = new mongoose.Schema({ name: String, price: Number, brand: String });
 
     const ProductModel = mongoose.model('products', ProductSchema);
-    let data = new ProductModel({ name: 'laptop', price: 1000000, brand:'new brand' });
+    let data = new ProductModel({ name: 'laptop', price: 1000000, brand: 'new brand' });
     let result = await data.save();
     console.log(result);
 
 
 }
-main();
+
+
+const updateInDB = async () => {
+    await mongoose.connect('mongodb://localhost:27017/E-comm');
+    const ProductSchema = new mongoose.Schema({ name: String, price: Number, brand: String });
+    const Product = mongoose.model('products', ProductSchema);
+    let data = await Product.updateOne({ name: 'laptop' }, { $set: { price: '80000' } });
+}
+updateInDB();
