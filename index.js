cf2effed9a1b8d5bb948ae9fbd90d70ad7bbdc5e
Aryan Sharma <panditaryan.as@gmail.com>
2023-01-01 11:53:12 +0530

    delete

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 
+mongoose.connect('mongodb://localhost:27017/E-comm');
+const ProductSchema = new mongoose.Schema({ name: String, price: Number, brand: String });
+
 const saveInDB = async () => {
-    await mongoose.connect('mongodb://localhost:27017/E-comm');
-    const ProductSchema = new mongoose.Schema({ name: String, price: Number, brand: String });
 
     const ProductModel = mongoose.model('products', ProductSchema);
     let data = new ProductModel({ name: 'laptop', price: 1000000, brand: 'new brand' });
@@ -14,9 +15,15 @@ const saveInDB = async () => {
 
 
 const updateInDB = async () => {
-    await mongoose.connect('mongodb://localhost:27017/E-comm');
-    const ProductSchema = new mongoose.Schema({ name: String, price: Number, brand: String });
     const Product = mongoose.model('products', ProductSchema);
     let data = await Product.updateOne({ name: 'laptop' }, { $set: { price: '80000' } });
 }
-updateInDB();
+// updateInDB();
+
+const deleteInDB = async () => {
+    const Product = mongoose.model('products',ProductSchema);
+    let data = await Product.deleteOne({id:'63b11fea87d970bed2d29e1d'});
+    console.log(data)
+}
+
+
