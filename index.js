abf232713d31889f9b87eb0a786bcc0ec92b0fe6
Aryan Sharma <panditaryan.as@gmail.com>
2023-01-01 13:18:35 +0530

    search-api

--- index.js
+++ index.js
@@ -6,31 +6,18 @@ const Products = require('./product');
 const app = express();
 app.use(express.json());
 
-app.post('/create', async (req, res) => {
-    let data = new Products(req.body);
-    let result = await data.save();
-    console.log(result)
-    res.send(result)
+app.get('/search/:key', async (req, res) => {
+    let data = await Products.find(
+        {
+            "$or": [
+                { "name": { $regex: req.params.key } },
+                { "brand": { $regex: req.params.key } },
+                { "category": { $regex: req.params.key } }
+            ]
+        }
+    );
+res.send(data)
 })
-
-app.get('/list', async (req, res) => {
-    let data = await Products.find();
-    res.send(data);
-
-})
-
-app.delete('/delete/:_id', async (req, res) => {
-    let data = await Products.deleteOne(req.params);
-    res.send(data);
-
-})
-
-app.put('/update/:_id', async (req, res) => {
-    let data = await Products.updateOne(req.params,{$set:{name:'y 20'}});
-    res.send(data);
-
-})
-
 
 
 
